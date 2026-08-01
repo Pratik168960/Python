f35d1a8c25e94fabbe23f487908d731278d46d45
Pratik168960 <pratiksingh111252@gmail.com>
2026-08-01 20:06:17 +0530

    how code executes

---
+++ how_code_executes.py
@@ -0,0 +1,14 @@
+
+print("Hello World")
+
+## how code executes 
+## python code gets executes line by line from the top 
+## interpreter is a program that knows how to translate or interpret
+## python code into instructions that a computer can understand 
+
+print('*' * 10)
+## here we are multiplying the string by number 10 
+## output : **********
+
+## when python interpreter tries to execute first it will evaluate the code 
+## that we put in between parenthesis 
