d3edbb15ea46959cab82894328416959c9d65f21
Pratik168960 <pratiksingh111252@gmail.com>
2026-08-02 07:38:49 +0530

    variables and data types basics

--- how_code_executes.py
+++ how_code_executes.py
@@ -1,14 +1,14 @@
 
 print("Hello World")
 
-## how code executes 
-## python code gets executes line by line from the top 
-## interpreter is a program that knows how to translate or interpret
-## python code into instructions that a computer can understand 
+# how code executes 
+# python code gets executes line by line from the top 
+# interpreter is a program that knows how to translate or interpret
+# python code into instructions that a computer can understand 
 
 print('*' * 10)
-## here we are multiplying the string by number 10 
-## output : **********
+# here we are multiplying the string by number 10 
+# output : **********
 
-## when python interpreter tries to execute first it will evaluate the code 
-## that we put in between parenthesis 
+# when python interpreter tries to execute first it will evaluate the code 
+# that we put in between parenthesis 
---
+++ variables.py
@@ -0,0 +1,19 @@
+# we use variables to temporarily store data in a computer's memory 
+
+price = 10 
+
+# python interpreter will allocate some memory then it will store 
+# number 10 in that memory and finally it will attach this price label to 
+# that memory location 
+
+print(price) # output : 10
+
+# when this 10 is about to store in the memory first it will get converted into 
+# binary representation computers only understand 0 and 1 then it will get stored in memory 
+
+# here we use integers number without decimal point
+# a number with decimal is referred to floating point number for short 
+price = 20          #integer 
+rating = 4.9        #float 
+name = "pratik"     #string
+is_active = False   #boolean
