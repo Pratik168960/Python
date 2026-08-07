27eb58206a08ed6bfdf3182d5b59e49899673487
Pratik168960 <pratiksingh111252@gmail.com>
2026-08-07 21:17:53 +0530

    function and keyword arguments

--- functions.py
+++ functions.py
@@ -1,6 +1,5 @@
-# ==========================================
+
 # FUNCTIONS: POSITIONAL & KEYWORD ARGUMENTS
-# ==========================================
 # Functions allow us to write reusable blocks of code.
 # We pass data into functions using parameters.
 
@@ -8,18 +7,14 @@ def greet_user(first_name, last_name):
     print(f"Hi {first_name} {last_name}!")
     print('Welcome aboard')
 
-# ------------------------------------------
 # 1. POSITIONAL ARGUMENTS
-# ------------------------------------------
-print("--- 1. POSITIONAL ARGUMENTS ---")
 # By default, arguments are assigned to parameters based on their position.
 # "Pratik" goes to first_name, "Singh" goes to last_name.
 greet_user("Pratik", "Singh")
 
-# ------------------------------------------
+
+
 # 2. KEYWORD ARGUMENTS
-# ------------------------------------------
-print("\n--- 2. KEYWORD ARGUMENTS ---")
 # We can explicitly state which parameter gets which value.
 # This allows us to ignore the order of the parameters!
 greet_user(last_name="Singh", first_name="Pratik")
@@ -31,19 +26,17 @@ greet_user(last_name="Singh", first_name="Pratik")
 
 '''
 Output:
---- 1. POSITIONAL ARGUMENTS ---
 Hi Pratik Singh!
 Welcome aboard
 
---- 2. KEYWORD ARGUMENTS ---
 Hi Pratik Singh!
 Welcome aboard
 '''
 
-# ------------------------------------------
+
+
 # 3. THE POWER OF KEYWORD ARGUMENTS
-# ------------------------------------------
-print("\n--- 3. WHY KEYWORD ARGUMENTS ROCK ---")
+
 
 def calc_cost(total, shipping, discount):
     final_price = total + shipping - (total * discount)
@@ -55,6 +48,5 @@ calc_cost(total=50, shipping=5, discount=0.1)
 
 '''
 Output:
---- 3. WHY KEYWORD ARGUMENTS ROCK ---
 Total cost: $50.0
 '''
